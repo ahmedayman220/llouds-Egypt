0e6efaa52c02befc20b4e6e5907a3c7cedd9cc06
ahmedayman220 <ahmedayman220@users.noreply.github.com>
2021-02-10 18:50:10 +0200

    kv

---
+++ menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(function() {
+        $('body').addClass('js');
+
+        var $hamburger = $('.hamburger'),
+            $nav = $('#site-nav'),
+            $masthead = $('#masthead');
+
+        $hamburger.click(function() {
+            $(this).toggleClass('is-active');
+            $nav.toggleClass('is-active');
+            $masthead.toggleClass('is-active');
+            return false;
+        })
+    });
+});
